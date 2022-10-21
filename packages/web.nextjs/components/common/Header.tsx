98e32d20f4b6f30f7124ef5fd74a6aeb7504470e
sk0x0y <seokyeong.yang@kakao.com>
2022-10-21 22:13:21 +0900

    feat(web-nextjs): Header 컴포넌트 추가

--- packages/web.nextjs/components/common/Header.tsx
+++ packages/web.nextjs/components/common/Header.tsx
@@ -1,7 +1,98 @@
-import React from 'react';
+import { css } from '@emotion/react';
+import Link from 'next/link';
+// import Image from 'next/image';
 
 function Header() {
-  return <header>Header Component</header>;
+  return (
+    <header>
+      <div
+        css={css`
+          width: 1050px;
+          margin: 0 auto;
+        `}
+      >
+        {/* 헤더 첫번째 라인 */}
+        <div
+          css={css`
+            display: flex;
+            align-items: center;
+            justify-content: flex-end;
+            font-size: 12px;
+          `}
+        >
+          <Link href="/">
+            <button
+              type="button"
+              css={css`
+                padding: 0;
+                border: 0;
+                background-color: transparent;
+                color: rgb(95, 0, 128);
+              `}
+            >
+              회원가입
+            </button>
+          </Link>
+
+          <div
+            css={css`
+              display: inline-block;
+              width: 1px;
+              height: 13px;
+              margin: 0 12px;
+              background-color: rgb(217, 217, 217);
+            `}
+          />
+
+          <Link href="/">
+            <button
+              type="button"
+              css={css`
+                padding: 0;
+                border: 0;
+                background-color: transparent;
+              `}
+            >
+              로그인
+            </button>
+          </Link>
+
+          <div
+            css={css`
+              display: inline-block;
+              width: 1px;
+              height: 13px;
+              margin: 0 12px;
+              background-color: rgb(217, 217, 217);
+            `}
+          />
+
+          <div
+            css={css`
+              line-height: 35px;
+            `}
+          >
+            <Link href="/">
+              <button
+                type="button"
+                css={css`
+                  padding: 0;
+                  border: 0;
+                  background-color: transparent;
+                `}
+              >
+                고객센터
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <div>2</div>
+
+      <div>3</div>
+    </header>
+  );
 }
 
 export default Header;
