944b86ef2b1a39d053149bd1effccf60848b7ef6
sk0x0y <seokyeong.yang@kakao.com>
2022-11-08 23:38:29 +0900

    feat(designsystem): add Button Anchor Component

---
+++ packages/web.nextjs/components/designsystem/button/index.ts
@@ -0,0 +1,15 @@
+import { css } from '@emotion/react';
+import Anchor from './Anchor';
+
+export const defaultStyle = css`
+  padding: 0;
+  border: 0;
+  background-color: transparent;
+  appearance: none;
+`;
+
+const Button = {
+  Anchor,
+};
+
+export default Button;
