a9a5e8d4e3f0a22786fdf10aae5bd9263596ccf9
sk0x0y <seokyeong.yang@kakao.com>
2022-11-11 23:27:10 +0900

    chore(web-nextjs): 메인 페이지 슬라이더 이미지 추가

---
+++ packages/web.nextjs/components/home/Carousel.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper as SwiperClass } from 'swiper/types';
+
+function Carousel() {
+  return (
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper: SwiperClass) => console.log(swiper)}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+    </Swiper>
+  );
+}
+
+export default Carousel;
