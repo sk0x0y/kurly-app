d070e01ba9b2c32e0e9ca1bd0687e222e8c12ca2
sk0x0y <seokyeong.yang@kakao.com>
2022-11-19 00:45:22 +0900

    chore(web-nextjs): Swiper 모듈 스타일 추가

--- packages/web.nextjs/pages/_app.tsx
+++ packages/web.nextjs/pages/_app.tsx
@@ -1,5 +1,11 @@
 import 'normalize.css';
 import '../styles/globals.css';
+// eslint-disable-next-line import/no-unresolved
+import 'swiper/scss';
+// eslint-disable-next-line import/no-unresolved
+import 'swiper/scss/navigation';
+// eslint-disable-next-line import/no-unresolved
+import 'swiper/scss/pagination';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
