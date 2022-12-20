2d7072e43b62a68beff1d1ed43ce3a32b11b82d2
sk0x0y <35445665+sk0x0y@users.noreply.github.com>
2022-12-21 04:10:52 +0900

    feature/setup-default-layout (#18)
    * feat(setup-default-layout): 레이아웃 생성 & 적용
    
    * chore(setup-default-layout): prettier/prettier disable

--- packages/web.nextjs/pages/_app.tsx
+++ packages/web.nextjs/pages/_app.tsx
@@ -7,9 +7,14 @@ import 'swiper/scss/navigation';
 // eslint-disable-next-line import/no-unresolved
 import 'swiper/scss/pagination';
 import type { AppProps } from 'next/app';
+import Layout from '../components/layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
