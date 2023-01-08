30461774a8ba3a602cfb37ea509d1d0acfe998ba
sk0x0y <seokyeong.yang@kakao.com>
2023-01-08 22:35:14 +0900

    chore(designsystem): Input.Common 컴포넌트 업데이트

--- packages/web.nextjs/components/designsystem/input/Common.tsx
+++ packages/web.nextjs/components/designsystem/input/Common.tsx
@@ -1,8 +1,8 @@
-import { css } from '@emotion/react';
+import { css, SerializedStyles } from '@emotion/react';
 import { InputHTMLAttributes } from 'react';
 
 interface IProps extends InputHTMLAttributes<HTMLInputElement> {
-
+  styles?: SerializedStyles;
   options?: {
     width?: number;
     height?: number;
@@ -16,23 +16,28 @@ interface IProps extends InputHTMLAttributes<HTMLInputElement> {
   };
 }
 function Common(props: IProps) {
-  const { options, placeholder } = props;
+  const { styles, options, id, placeholder, required } = props;
 
   return (
     <input
-      placeholder={placeholder}
-      css={css`
-        width: ${options?.width ?? '100%'};
-        height: ${options?.height ?? 46}px;
-        padding: ${options?.padding ?? '0 11px 1px 15px'};
-        border: ${options?.border ?? '1px solid rgb(221, 221, 221)'};
-        border-radius: ${options?.borderRadius ?? 4}px;
-        outline: none;
-        font-weight: ${options?.fontWeight ?? 400};
-        font-size: ${options?.fontSize ?? 16}px;
-        color: ${options?.color ?? 'rgb(51, 51, 51)'};
-        line-height: ${options?.lineHeight ?? 1.5};
-      `}
+      id={id}
+      placeholder={placeholder ?? 'Placeholder'}
+      css={[
+        css`
+          width: ${options?.width ?? '100%'};
+          height: ${options?.height ?? 46}px;
+          padding: ${options?.padding ?? '0 11px 1px 15px'};
+          border: ${options?.border ?? '1px solid rgb(221, 221, 221)'};
+          border-radius: ${options?.borderRadius ?? 4}px;
+          outline: none;
+          font-weight: ${options?.fontWeight ?? 400};
+          font-size: ${options?.fontSize ?? 16}px;
+          color: ${options?.color ?? 'rgb(51, 51, 51)'};
+          line-height: ${options?.lineHeight ?? 1.5};
+        `,
+        styles,
+      ]}
+      required={required}
     />
   );
 }
