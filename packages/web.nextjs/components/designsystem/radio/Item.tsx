d79f128307765e9d7977153382ab55f088ca23f1
sk0x0y <seokyeong.yang@kakao.com>
2023-01-08 22:36:07 +0900

    feat(designsystem): Radio.Item 및 Radio.Group 컴포넌트 추가

---
+++ packages/web.nextjs/components/designsystem/radio/Item.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { css, SerializedStyles } from '@emotion/react';
+
+interface IProps {
+  id?: string;
+  checked?: {
+    styles?: SerializedStyles;
+  };
+  children?: React.ReactNode;
+}
+function Item(props: IProps) {
+  const { id, checked, children } = props;
+
+  return (
+    <label
+      htmlFor={id}
+      css={css`
+        display: flex;
+        align-items: center;
+        padding: 8px 0;
+      `}
+    >
+      <input
+        id={id}
+        type="radio"
+        css={css`
+          appearance: none;
+          width: 24px;
+          height: 24px;
+          border: 1px solid rgb(221, 221, 221);
+          border-radius: 50%;
+
+          &:checked {
+            ${checked?.styles}
+          }
+        `}
+      />
+      {children}
+    </label>
+  );
+}
+
+export default Item;
