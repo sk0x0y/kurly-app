1cf87046bfaf74ac0f914cb518cc98c6893769f6
sk0x0y <seokyeong.yang@kakao.com>
2023-01-17 21:31:14 +0900

    feat(designsystem): Input.CheckBox 컴포넌트 추가

--- packages/web.nextjs/components/designsystem/input/index.ts
+++ packages/web.nextjs/components/designsystem/input/index.ts
@@ -1,10 +1,12 @@
 import { css } from '@emotion/react';
 import Common from './Common';
+import CheckBox from './checkbox';
 
 export const defaultStyle = css``;
 
 const Input = {
   Common,
+  CheckBox,
 };
 
 export default Input;
