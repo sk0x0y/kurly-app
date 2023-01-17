61cd39cbd0872434eb4020543af76c2058ef17ef
sk0x0y <35445665+sk0x0y@users.noreply.github.com>
2023-01-17 21:49:52 +0900

    feature/designsystem (#29)
    * chore(designsystem): Radio 컴포넌트 제거
    
    * chore(designsystem): Input 하위 컴포넌트로 변경
    
    * feat(designsystem): Input.CheckBox 컴포넌트 추가

---
+++ packages/web.nextjs/components/designsystem/input/checkbox/Item.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { css, SerializedStyles } from '@emotion/react';
+
+interface IProps {
+  id?: string;
+  styles?: {
+    default?: SerializedStyles;
+    checked?: SerializedStyles;
+  };
+  children?: React.ReactNode;
+}
+function Item(props: IProps) {
+  const { id, styles, children } = props;
+
+  return (
+    <label
+      htmlFor={id}
+      css={css`
+        display: flex;
+        align-items: center;
+        padding: 8px 0;
+      `}
+    >
+      <input
+        id={id}
+        type="checkbox"
+        css={[
+          css`
+            appearance: none;
+            width: 24px;
+            height: 24px;
+            border: 1px solid rgb(221, 221, 221);
+            border-radius: 50%;
+          `,
+          css`
+            ${styles?.default}
+          `,
+          css`
+            &:checked {
+              ${styles?.checked}
+            }
+          `,
+        ]}
+      />
+      {children}
+    </label>
+  );
+}
+
+export default Item;
