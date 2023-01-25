7c6cfb40e596bea73ecc3bca6eb3faacbc38fc6d
sk0x0y <seokyeong.yang@kakao.com>
2023-01-25 21:27:20 +0900

    chore(designsystem/kernel-product-ui): change entity id

--- packages/web.nextjs/components/header/_entity/CommonCenterMenu.entity.ts
+++ packages/web.nextjs/components/header/_entity/CommonCenterMenu.entity.ts
@@ -10,9 +10,9 @@ export default class CommonCenterMenuEntity {
   constructor() {
     this._entity = [
       { id: 1, name: '신상품', url: '/market/new' },
-      { id: 1, name: '베스트', url: '/market/best' },
-      { id: 1, name: '알뜰쇼핑', url: '/market/budget' },
-      { id: 1, name: '특가/혜택', url: '/market/promotion' },
+      { id: 2, name: '베스트', url: '/market/best' },
+      { id: 3, name: '알뜰쇼핑', url: '/market/budget' },
+      { id: 4, name: '특가/혜택', url: '/market/promotion' },
     ];
   }
 }
