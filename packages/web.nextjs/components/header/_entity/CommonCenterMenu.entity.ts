d37a40c6a9816f1724a2fc2710dc9c21f7c99f1c
sk0x0y <seokyeong.yang@kakao.com>
2023-01-26 22:47:36 +0900

    chore(designsystem/kernel-product-ui): new 키워드는 지양해야 하므로 new-product 로 url 변경

--- packages/web.nextjs/components/header/_entity/CommonCenterMenu.entity.ts
+++ packages/web.nextjs/components/header/_entity/CommonCenterMenu.entity.ts
@@ -9,7 +9,7 @@ export default class CommonCenterMenuEntity {
 
   constructor() {
     this._entity = [
-      { id: 1, name: '신상품', url: '/market/new' },
+      { id: 1, name: '신상품', url: '/market/new-product' },
       { id: 2, name: '베스트', url: '/market/best' },
       { id: 3, name: '알뜰쇼핑', url: '/market/budget' },
       { id: 4, name: '특가/혜택', url: '/market/promotion' },
