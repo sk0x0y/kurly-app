5df2fd9a2ac5fb7fe4693b2d6a44050f6e2b16c0
sk0x0y <seokyeong.yang@kakao.com>
2023-02-20 21:17:56 +0900

    refactor(designsystem/market-promotion-page): Content.Banner Component

--- packages/web.nextjs/components/designsystem/content/Banner.tsx
+++ packages/web.nextjs/components/designsystem/content/Banner.tsx
@@ -1,29 +1,34 @@
-import { css } from '@emotion/react';
+import { css, SerializedStyles } from '@emotion/react';
+import { IBanner } from '../../../infrastructure/interface/banner.interface';
 
-interface IProps {
-  src: string;
+interface IProps extends Omit<IBanner, 'id'> {
+  styles?: SerializedStyles;
 }
 function Banner(props: IProps) {
-  const { src } = props;
+  const { url, image, styles } = props;
 
   return (
     <section
-      css={css`
-        width: 1050px;
-        margin: 0 auto;
-        padding: 40px 0;
-      `}
+      css={[
+        css`
+          width: ${image?.size?.width ?? 1050}px;
+          height: ${image?.size?.height}px;
+          margin: 0 auto;
+          padding: 40px 0;
+        `,
+        styles,
+      ]}
     >
       <a
-        href="https://www.kurly.com/main/beauty"
+        href={url ?? 'https://www.kurly.com/main/beauty'}
         css={css`
           display: block;
-          height: 140px;
+          height: ${image?.size?.height ?? 140}px;
         `}
       >
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
-          src={src}
+          src={image?.src}
           alt=""
           css={css`
             width: 100%;
