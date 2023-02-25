f8386fcaae872b9d38254d415fa9c5e5188fd6bd
sk0x0y <seokyeong.yang@kakao.com>
2023-02-25 20:34:23 +0900

    chore(designsystem/product-detail-component): update product.local usecase

--- packages/web.nextjs/infrastructure/interactor/local/product.local.interactor.ts
+++ packages/web.nextjs/infrastructure/interactor/local/product.local.interactor.ts
@@ -1,10 +1,10 @@
-import { ProductDTO } from '../../../application/dto/product.dto';
+// import { ProductDTO } from '../../../application/dto/product.dto';
 import { ProductLocalUsecase } from '../../../application/usecases/local/product.local.usecase';
 
 export class ProductLocalInteractor {
   constructor(private readonly productLocalUsecase: ProductLocalUsecase) {}
 
-  hello(category?: ProductDTO) {
-    return this.productLocalUsecase.hello(category);
+  find(productId: number) {
+    return this.productLocalUsecase.find(productId);
   }
 }
