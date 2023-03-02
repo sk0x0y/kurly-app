d15e873d45c787b78d5ba2bdcc6e8cb676c7232b
sk0x0y <seokyeong.yang@kakao.com>
2023-03-03 04:02:32 +0900

    chore(designsystem/product-detail-component): update new-product.tsx

--- packages/web.nextjs/infrastructure/interface/product.interface.ts
+++ packages/web.nextjs/infrastructure/interface/product.interface.ts
@@ -3,6 +3,26 @@ export const DeliverType = {
   SLOW: 1, // NOTE: 택배배송
 } as const;
 
+export type Paragraph = {
+  id: number;
+  title?: string;
+  text?: string;
+  description?: { weight: string; characteristics: string };
+};
+export interface IContent {
+  title?: string;
+  subTitle?: string;
+  image?: {
+    id: number;
+    src: string;
+    width?: string;
+    height?: number;
+    paragraph?: Paragraph[];
+  }[];
+  description?: string;
+}
+export type IntroType = { image?: { src: string } } & Omit<IContent, 'image'>;
+
 export interface IProduct {
   id: number;
   src?: string; // DEPRECATED: 구 이미지 구조 will be deprecated
@@ -23,6 +43,14 @@ export interface IProduct {
     };
   };
   notice?: { id: number; label: string; title: string; description?: string }[];
+  content?: {
+    intro?: IntroType;
+    checkpoint?: IContent;
+    pick?: IContent;
+    tip?: IContent;
+    note?: IContent;
+    about?: IContent;
+  };
   review?: {
     count: number;
   };
