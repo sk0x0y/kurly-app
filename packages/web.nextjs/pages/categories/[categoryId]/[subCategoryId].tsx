7d3a4b7cc6ada8023bac4b3fff814c5d797ba4e5
sk0x0y <35445665+sk0x0y@users.noreply.github.com>
2023-03-12 03:53:32 +0900

    feature/category-page (#53)
    * chore(feature/category-page): scaffold component
    
    * chore(feature/category-page): UI 적용 및 router.query 확인용 엘리먼트 작성

---
+++ packages/web.nextjs/pages/categories/[categoryId]/[subCategoryId].tsx
@@ -0,0 +1,93 @@
+import { css } from '@emotion/react';
+import Content from '../../../components/designsystem/content';
+import Typography from '../../../components/designsystem/typography';
+import Product from '../../../components/designsystem/product';
+import Kernel from '../../../components/designsystem/kernel';
+import Pagination from '../../../components/designsystem/pagination/Pagination';
+import ItemSortTab from '../../../components/designsystem/tab/ItemSortTab';
+// eslint-disable-next-line no-unused-vars
+import {
+  useLocalCategoryEntity,
+  useLocalNewProductEntity,
+  useLocalProductEntity,
+} from '../../../infrastructure/zustand';
+import { useRouter } from 'next/router';
+
+function SubCategoryId() {
+  const router = useRouter();
+  const categoryId = router.query.categoryId as string;
+  const subCategoryId = router.query.subCategoryId as string;
+  const categoryEntity = useLocalCategoryEntity();
+  // const productEntity = useLocalProductEntity();
+  const productEntity = useLocalNewProductEntity();
+
+  // TODO: 추후 react-query 로 API Fetch 해서 Entity 에 Dispatch 해 줄 예정
+
+  return (
+    <Content.Section>
+      <Typography.Title
+        level="h2"
+        styles={css`
+          font-size: 28px;
+          text-align: center;
+          letter-spacing: -1px;
+        `}
+      >
+        채소
+      </Typography.Title>
+
+      <div>
+        <p>CategoryID: {categoryId}</p>
+        <p>SubCategoryID: {subCategoryId}</p>
+      </div>
+
+      <div
+        css={css`
+          display: flex;
+          margin-top: 50px;
+        `}
+      >
+        {/* 필터 및 초기화 */}
+        <Kernel.Product.FilterContainer>
+          {/* 필터 아이템(항목) */}
+          <Kernel.Product.FilterGroup name="카테고리" entities={categoryEntity} expand />
+
+          <Kernel.Product.FilterGroup name="브랜드" entities={categoryEntity} />
+
+          <Kernel.Product.FilterGroup name="가격" entities={categoryEntity} />
+
+          <Kernel.Product.FilterGroup name="혜택" entities={categoryEntity} />
+
+          <Kernel.Product.FilterGroup name="유형" entities={categoryEntity} />
+        </Kernel.Product.FilterContainer>
+
+        {/* 정렬탭, 아이템(상품) 리스트, 페이지네이션 */}
+        <div
+          css={css`
+            width: 100%;
+          `}
+        >
+          <ItemSortTab />
+          <div
+            css={css`
+              display: grid;
+              grid-template-columns: repeat(auto-fill, 249px);
+              gap: 31px 18px;
+            `}
+          >
+            {productEntity.map(product => (
+              <Product.Collection
+                key={product.id}
+                product={product}
+                options={{ imageSize: { width: 249, height: 320 } }}
+              />
+            ))}
+          </div>
+          <Pagination />
+        </div>
+      </div>
+    </Content.Section>
+  );
+}
+
+export default SubCategoryId;
