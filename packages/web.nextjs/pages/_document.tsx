10301705e6b5364b988beb936e1c2572d512f730
sk0x0y <seokyeong.yang@kakao.com>
2023-03-22 14:37:14 +0900

    feat(feature/category-page): apply webfont via _document

---
+++ packages/web.nextjs/pages/_document.tsx
@@ -0,0 +1,23 @@
+import { Head, Html, Main, NextScript } from 'next/document';
+
+function Document() {
+  return (
+    <Html lang="ko">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Gothic+A1:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+export default Document;
