50b17c9f227907429232c41e49f26721bb2cdb75
sk0x0y <seokyeong.yang@kakao.com>
2023-03-30 04:21:03 +0900

    chore(feature/select-product-put-in-cart): update Group.tsx

--- packages/web.nextjs/components/designsystem/select/Group.tsx
+++ packages/web.nextjs/components/designsystem/select/Group.tsx
@@ -1,64 +1,81 @@
 import { css } from '@emotion/react';
 import { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Select from './index';
 import Button from '../button';
 import { RootState } from '../../../infrastructure/redux';
+import { actions } from '../../../application/usecases/local/selectedProduct.local.usecase';
 
 function Group() {
-  const [isExpanded, setIsExpanded] = useState(false);
+  const dispatch = useDispatch();
   const productEntity = useSelector((state: RootState) => state.newProductAdaptor.entity);
-  const [selectedEntity, setSelectedEntity] = useState([]);
+  const selectedProductEntity = useSelector((state: RootState) => state.selectedProductAdaptor.entity);
+  const [isExpanded, setIsExpanded] = useState(false);
 
   return (
-    <div
-      css={css`
-        position: relative;
-      `}
-    >
+    <>
       <Button.Base
-        onClick={() => setIsExpanded(!isExpanded)}
+        onClick={() => {
+          setIsExpanded(false);
+        }}
         styles={css`
-          display: inline-flex;
-          align-items: center;
-          width: 100%;
-          height: 40px;
-          padding: 0 14px;
-          border: 1px solid rgb(221, 221, 221);
-          font-size: 12px;
-          line-height: 1.4375em;
-          letter-spacing: 0.00938em;
-          cursor: pointer;
-          user-select: none;
+          position: ${isExpanded && 'fixed'};
+          top: 0;
+          right: 0;
+          bottom: 0;
+          left: 0;
+          cursor: initial;
+        `}
+        nomessage
+      />
+      <div
+        css={css`
+          position: relative;
         `}
       >
-        <p>상품을 선택해주세요</p>
-        <i
-          css={css`
-            position: absolute;
-            right: 10px;
-            display: inline-block;
-            width: 24px;
-            height: 24px;
-            background-image: url('data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PSIwIDAgMjQgMjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTcgMTBsNSA1IDUtNXoiIC8+PC9zdmc+Cg==');
-            transform: ${isExpanded && 'rotate(180deg)'};
+        <Button.Base
+          onClick={() => setIsExpanded(!isExpanded)}
+          styles={css`
+            display: inline-flex;
+            align-items: center;
+            width: 100%;
+            height: 40px;
+            padding: 0 14px;
+            border: 1px solid rgb(221, 221, 221);
+            font-size: 12px;
+            line-height: 1.4375em;
+            letter-spacing: 0.00938em;
+            cursor: pointer;
+            user-select: none;
           `}
+        >
+          <p>상품을 선택해주세요</p>
+          <i
+            css={css`
+              position: absolute;
+              right: 10px;
+              display: inline-block;
+              width: 24px;
+              height: 24px;
+              background-image: url('data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PSIwIDAgMjQgMjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTcgMTBsNSA1IDUtNXoiIC8+PC9zdmc+Cg==');
+              transform: ${isExpanded && 'rotate(180deg)'};
+            `}
+          />
+        </Button.Base>
+
+        <Select.List
+          isExpanded={isExpanded}
+          productEntity={productEntity}
+          onClick={subProduct => {
+            setIsExpanded(false);
+            dispatch(actions.select(subProduct));
+          }}
         />
-      </Button.Base>
 
-      <Select.List
-        isExpanded={isExpanded}
-        productEntity={productEntity}
-        onClick={subProduct => {
-          setSelectedEntity([]);
-
-          console.log(selectedEntity);
-        }}
-      />
-
-      {/* 선택 된 리스트 아이템을 보여주는 컴포넌트 */}
-      <Select.SelectedItem selectedEntity={selectedEntity} />
-    </div>
+        {/* 선택 된 리스트 아이템을 보여주는 컴포넌트 */}
+        <Select.SelectedItem selectedProductEntity={selectedProductEntity} />
+      </div>
+    </>
   );
 }
 
