21c026c817dad8ee5531fbf4e0bc337385370f92
sk0x0y <seokyeong.yang@kakao.com>
2023-03-30 04:46:20 +0900

    feat(feature/select-product-put-in-cart): 장바구니 담기 기능 추가

--- packages/web.nextjs/application/usecases/local/cart.local.usecase.ts
+++ packages/web.nextjs/application/usecases/local/cart.local.usecase.ts
@@ -1,6 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { CartLocalEntity } from '../../entities/local/cart.local.entity';
-import { ICart } from '../../../infrastructure/interface/cart.interface';
+import { ISelectedProduct } from '../../../infrastructure/interface/selectedProduct.interface';
 
 const initialState: CartLocalEntity = {
   ...new CartLocalEntity(),
@@ -10,7 +10,13 @@ const cartSlice = createSlice({
   initialState,
   name: 'cart',
   reducers: {
-    put: (state, action: PayloadAction<ICart[]>) => {
+    put: (state, action: PayloadAction<ISelectedProduct[]>) => {
+      const isFill = action.payload.length;
+
+      if (!isFill) {
+        window.alert('선택한 상품이 없습니다.');
+      }
+
       state.entity = [...state.entity, ...action.payload];
     },
   },
