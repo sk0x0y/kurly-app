77479a918334028131fb0366fb06f57d1cecd0af
sk0x0y <seokyeong.yang@kakao.com>
2023-03-30 04:47:25 +0900

    chore(feature/select-product-put-in-cart): register cart slice

--- packages/web.nextjs/infrastructure/redux/index.ts
+++ packages/web.nextjs/infrastructure/redux/index.ts
@@ -1,19 +1,22 @@
 import { HYDRATE } from 'next-redux-wrapper';
 import { AnyAction, CombinedState, combineReducers } from '@reduxjs/toolkit';
+import { reducer as cartReducer } from '../../application/usecases/local/cart.local.usecase';
 import { reducer as categoryReducer } from '../../application/usecases/local/category.local.usecase';
 import { reducer as productReducer } from '../../application/usecases/local/product.local.usecase';
 import { reducer as selectedProductReducer } from '../../application/usecases/local/selectedProduct.local.usecase';
 import { reducer as newProductReducer } from '../../application/usecases/local/newProduct.local.usecase';
 import { reducer as specialDealReducer } from '../../application/usecases/local/specialDeal.local.usecase';
 import { reducer as bannerReducer } from '../../application/usecases/local/banner.local.usecase';
-import { SelectedProductLocalEntity } from '../../application/entities/local/selectedProduct.local.entity';
+import { CartLocalEntity } from '../../application/entities/local/cart.local.entity';
+import { CategoryLocalEntity } from '../../application/entities/local/category.local.entity';
 import { ProductLocalEntity } from '../../application/entities/local/product.local.entity';
 import { NewProductLocalEntity } from '../../application/entities/local/newProduct.local.entity';
 import { SpecialDealLocalEntity } from '../../application/entities/local/specialDeal.local.entity';
-import { CategoryLocalEntity } from '../../application/entities/local/category.local.entity';
+import { SelectedProductLocalEntity } from '../../application/entities/local/selectedProduct.local.entity';
 import { BannerLocalEntity } from '../../application/entities/local/banner.local.entity';
 
 interface ReducerState {
+  cartAdaptor: CartLocalEntity;
   categoryAdaptor: CategoryLocalEntity;
   productAdaptor: ProductLocalEntity;
   newProductAdaptor: NewProductLocalEntity;
@@ -33,6 +36,7 @@ export const rootReducer = (state: any, action: AnyAction): CombinedState<Reduce
 
     default:
       return combineReducers({
+        cartAdaptor: cartReducer,
         categoryAdaptor: categoryReducer,
         productAdaptor: productReducer,
         newProductAdaptor: newProductReducer,
