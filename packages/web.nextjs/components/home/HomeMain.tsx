d37ad445dae7ebdfc668e5a8bca5e56e7785789e
sk0x0y <35445665+sk0x0y@users.noreply.github.com>
2023-04-14 19:03:42 +0900

    feature/wwka/today-recommendation (#64)
    * feat(feature/wwka/today-recommendation): add custom hook, api function for today recommendation
    
    * chore(feature/wwka/today-recommendation): update product interface
    
    * feat(feature/wwka/today-recommendation): change workflow to today recommendation hook
    
    * feat(feature/wwka/today-recommendation): change code usage to origin kurly api state
    
    * feat(feature/wwka/today-recommendation): apply changed new usage

--- packages/web.nextjs/components/home/HomeMain.tsx
+++ packages/web.nextjs/components/home/HomeMain.tsx
@@ -1,8 +1,13 @@
+import { AxiosResponse } from 'axios';
 import Product from '../designsystem/product';
 import Content from '../designsystem/content';
 import Carousel from './Carousel';
+import { useTodayRecommendation } from '../../hooks';
+import { IKurlyProduct } from '../../infrastructure/interface/product.interface';
 
 function HomeMain() {
+  const { data: products, isLoading } = useTodayRecommendation();
+
   return (
     <>
       {/* Swiper Container - 메인 슬라이더 */}
@@ -10,7 +15,8 @@ function HomeMain() {
         <Carousel />
       </div>
 
-      <Product.Section title="이 상품 어때요?" options={{ imageSize: { width: 249, height: 320 } }} />
+      {/* <Product.Section title="이 상품 어때요?" options={{ imageSize: { width: 249, height: 320 } }} /> */}
+      <Product.Section products={products as AxiosResponse<IKurlyProduct>} />
 
       <Content.Banner
         image={{
@@ -20,20 +26,20 @@ function HomeMain() {
 
       <Product.SpecialDeal options={{ imageSize: { width: 338, height: 434 } }} />
 
-      <Product.Section
-        title="놓치면 후회할 가격"
-        href="https://www.kurly.com"
-        options={{ imageSize: { width: 249, height: 320 } }}
-      />
+      {/* <Product.Section */}
+      {/*  title="놓치면 후회할 가격" */}
+      {/*  href="https://www.kurly.com" */}
+      {/*  options={{ imageSize: { width: 249, height: 320 } }} */}
+      {/* /> */}
 
       <Content.Banner image={{ src: '/banner/7209c0f1-d06b-4c2c-921d-2edba1290fc2.jpg' }} />
 
-      <Product.Section
-        title="6년차 워킹맘이 자주 구매하는"
-        subTitle="컬리 MD가 직접 구매하는 상품들을 추천해요"
-        href="https://www.kurly.com"
-        options={{ imageSize: { width: 249, height: 320 } }}
-      />
+      {/* <Product.Section */}
+      {/*  title="6년차 워킹맘이 자주 구매하는" */}
+      {/*  subTitle="컬리 MD가 직접 구매하는 상품들을 추천해요" */}
+      {/*  href="https://www.kurly.com" */}
+      {/*  options={{ imageSize: { width: 249, height: 320 } }} */}
+      {/* /> */}
 
       <Product.MerchandiserChoice options={{ imageSize: { width: 249, height: 320 } }} />
     </>
