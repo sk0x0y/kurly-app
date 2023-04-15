59e6e8e6100517e5e64eff7b586912efc7cec4be
sk0x0y <seokyeong.yang@kakao.com>
2023-04-15 21:56:26 +0900

    chore(feature/wwka/random-collection): update interfaces

--- packages/web.nextjs/infrastructure/interface/banner.interface.ts
+++ packages/web.nextjs/infrastructure/interface/banner.interface.ts
@@ -6,16 +6,7 @@ export interface IKurlyBannerData {
   link: string;
   background_color?: string;
 }
-export interface IKurlyBanner {
-  id: number;
-  title: string;
-  subtitle: string;
-  additional_text: null;
-  template_code: 'main_banner' | 'random_line_banner';
-  template_type: 'main_banner' | 'random_line_banner';
-  event_code: 'main_banner' | 'random_line_banner';
-  data: IKurlyBannerData[];
-}
+
 export interface IBanner {
   id: number;
   url?: string;
---
+++ packages/web.nextjs/infrastructure/interface/merchandiserChoice.interface.ts
@@ -0,0 +1,8 @@
+export interface IMerchandiserChoiceData {
+  default_code: '722';
+  md_choices: {
+    code: '907';
+    name: string;
+    link: string;
+  }[];
+}
