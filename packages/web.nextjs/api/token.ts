24f059bdfd418c14a672d1efbf2675e81e9df5b2
sk0x0y <seokyeong.yang@kakao.com>
2023-04-16 00:10:18 +0900

    fix(develop): try to fix cors

--- packages/web.nextjs/api/token.ts
+++ packages/web.nextjs/api/token.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export function getToken() {
-  return axios.get(process.env.NODE_ENV === 'production' ? 'https://www.kurly.com/nx/api/session' : '/nx/api/session');
+  // return axios.get(process.env.NODE_ENV === 'production' ? 'https://www.kurly.com/nx/api/session' : '/nx/api/session');
+  return axios.get('/nx/api/session');
 }
