f7c81fe7e4888bafe7dfe6186ae7bfa7c3044150
sk0x0y <seokyeong.yang@kakao.com>
2023-04-18 01:50:21 +0900

    feat(feature/wwka/category-group): apply conditional icon via using kurly api

--- packages/web.nextjs/components/header/HamburgerMenu.tsx
+++ packages/web.nextjs/components/header/HamburgerMenu.tsx
@@ -2,11 +2,10 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
 import { css } from '@emotion/react';
-import { IHamburgerMenu } from '../../types/menu/HamburgerMenu.interface';
-import HamburgerMenuEntity from './_entity/HamburgerMenu.entity';
+import { useCategoryGroup } from '../../hooks';
 
 function HamburgerMenu() {
-  const [hamburgerMenuList] = useState<IHamburgerMenu[]>(new HamburgerMenuEntity().entity);
+  const { data: categories } = useCategoryGroup();
   const [currentCursor, setCurrentCursor] = useState<number>(0);
   // Presenter(Controller) -> DTO -> Entity
 
@@ -32,96 +31,97 @@ function HamburgerMenu() {
           overflow-y: scroll;
         `}
       >
-        {hamburgerMenuList.map(item => (
-          <Link key={item.id} href={item.url}>
-            <li
-              onMouseEnter={() => {
-                setCurrentCursor(item.id);
-              }}
-              onMouseLeave={() => {
-                setCurrentCursor(0);
-              }}
-              css={css`
-                list-style: none;
-                padding: 7px 0 9px 14px;
-
-                &:hover {
-                  background-color: rgb(247, 247, 247);
-                  color: rgb(95, 0, 128);
-                }
-              `}
-            >
-              <div
+        {categories &&
+          categories.data.data.main.map(item => (
+            <Link key={item.code} href={item.code}>
+              <li
+                onMouseEnter={() => {
+                  setCurrentCursor(Number(item.code));
+                }}
+                onMouseLeave={() => {
+                  setCurrentCursor(0);
+                }}
                 css={css`
-                  display: flex;
-                  flex-wrap: wrap;
-                  align-items: flex-start;
+                  list-style: none;
+                  padding: 7px 0 9px 14px;
+
+                  &:hover {
+                    background-color: rgb(247, 247, 247);
+                    color: rgb(95, 0, 128);
+                  }
                 `}
               >
-                <Image
-                  src={currentCursor === item.id ? item.image.active || item.image.default : item.image.default}
-                  alt={item.name}
-                  width={24}
-                  height={24}
-                  style={{ flex: '0 1 0%' }}
-                />
-                <span
+                <div
                   css={css`
-                    font-weight: ${currentCursor === item.id && 500};
-                    padding: 1px 20px 0 10px;
-                    line-height: 22px;
+                    position: relative;
+                    display: flex;
+                    flex-wrap: wrap;
+                    align-items: flex-start;
                   `}
                 >
-                  {item.name}
-                </span>
-              </div>
-              {currentCursor === item.id && (
-                <ul
-                  css={css`
-                    position: absolute;
-                    top: 0;
-                    left: 250px;
-                    z-index: 1000;
-                    width: 266px;
-                    height: 100%;
-                    padding: 0;
-                    background-color: rgb(247, 247, 247);
-                    overflow-y: scroll;
-                    list-style: none;
-                  `}
-                >
-                  {item.children?.map(child => (
-                    <Link key={child.id} href={child.url}>
-                      <li
-                        css={css`
-                          display: flex;
-                          align-items: center;
-                          padding: 11px 20px 11px 18px;
-                          line-height: 22px;
-
-                          &:hover > span {
-                            font-weight: 500;
-                            color: rgb(95, 0, 128);
-                            text-decoration: underline;
-                          }
-                        `}
-                      >
-                        <span
+                  <Image
+                    src={currentCursor === Number(item.code) ? item.pc_icon_active_url : item.pc_icon_url}
+                    alt={item.name}
+                    width={24}
+                    height={24}
+                  />
+                  <span
+                    css={css`
+                      font-weight: ${currentCursor === Number(item.code) && 500};
+                      padding: 1px 20px 0 10px;
+                      line-height: 22px;
+                    `}
+                  >
+                    {item.name}
+                  </span>
+                </div>
+                {currentCursor === Number(item.code) && (
+                  <ul
+                    css={css`
+                      position: absolute;
+                      top: 0;
+                      left: 250px;
+                      z-index: 1000;
+                      width: 266px;
+                      height: 100%;
+                      padding: 0;
+                      background-color: rgb(247, 247, 247);
+                      overflow-y: scroll;
+                      list-style: none;
+                    `}
+                  >
+                    {item.sub_category_groups.map(child => (
+                      <Link key={child.code} href={child.code}>
+                        <li
                           css={css`
-                            line-height: 18px;
-                            color: rgb(51, 51, 51);
+                            display: flex;
+                            align-items: center;
+                            padding: 11px 20px 11px 18px;
+                            line-height: 22px;
+
+                            &:hover > span {
+                              font-weight: 500;
+                              color: rgb(95, 0, 128);
+                              text-decoration: underline;
+                            }
                           `}
                         >
-                          {child.name}
-                        </span>
-                      </li>
-                    </Link>
-                  ))}
-                </ul>
-              )}
-            </li>
-          </Link>
-        ))}
+                          <span
+                            css={css`
+                              line-height: 18px;
+                              color: rgb(51, 51, 51);
+                            `}
+                          >
+                            {child.name}
+                          </span>
+                        </li>
+                      </Link>
+                    ))}
+                  </ul>
+                )}
+              </li>
+            </Link>
+          ))}
       </ul>
     </div>
   );
