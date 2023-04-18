5b83a6c41c27a013113db3854f39c8e87f7541d0
sk0x0y <seokyeong.yang@kakao.com>
2023-04-18 14:51:54 +0900

    fix(feature/wwka/category-product): fix kurly api url

--- packages/web.nextjs/api/category/category-product.ts
+++ packages/web.nextjs/api/category/category-product.ts
@@ -1,7 +1,10 @@
 import { axiosInstance } from '../index';
 
 export function getCategoryProduct(token: string, categoryId: string) {
-  return axiosInstance.get(`/collection/v2/home/product-categories/${categoryId}`, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
+  return axiosInstance.get(
+    `/collection/v2/home/product-categories/${categoryId}/products?sort_type=4&page=1&per_page=15&filters=`,
+    {
+      headers: { Authorization: `Bearer ${token}` },
+    }
+  );
 }
