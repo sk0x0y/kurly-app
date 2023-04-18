2bd06d4785c59590d08c2fbd8beffed600fa2050
sk0x0y <seokyeong.yang@kakao.com>
2023-04-19 05:48:52 +0900

    feat(feature/wwka/category-product): 코드 정리 및 새로운 컬렉션 필터 훅 적용, 필터 컴포넌트 반복 렌더링

--- packages/web.nextjs/pages/market/best.tsx
+++ packages/web.nextjs/pages/market/best.tsx
@@ -1,20 +1,15 @@
 import { css } from '@emotion/react';
-import { useSelector } from 'react-redux';
 import Content from '../../components/designsystem/content';
 import Typography from '../../components/designsystem/typography';
 import Product from '../../components/designsystem/product';
 import Kernel from '../../components/designsystem/kernel';
 import Pagination from '../../components/designsystem/pagination/Pagination';
 import ItemSortTab from '../../components/designsystem/tab/ItemSortTab';
-import { RootState } from '../../infrastructure/redux';
-import { useBestProduct } from '../../hooks';
-import { IKurlyProductData } from '../../infrastructure/interface/product.interface';
+import { useBestProduct, useCollectionFilter } from '../../hooks';
 
 function Best() {
-  const categoryEntity = useSelector((state: RootState) => state.categoryAdaptor.entity);
-  // const productEntity = useLocalProductEntity();
-  // const productEntity = useSelector((state: RootState) => state.newProductAdaptor.entity);
   const { data: products } = useBestProduct();
+  const { data: filters } = useCollectionFilter('market-best');
 
   // TODO: 추후 react-query 로 API Fetch 해서 Entity 에 Dispatch 해 줄 예정
 
@@ -40,15 +35,10 @@ function Best() {
         {/* 필터 및 초기화 */}
         <Kernel.Product.FilterContainer>
           {/* 필터 아이템(항목) */}
-          <Kernel.Product.FilterGroup name="카테고리" entities={categoryEntity} expand />
-
-          <Kernel.Product.FilterGroup name="브랜드" entities={categoryEntity} />
-
-          <Kernel.Product.FilterGroup name="가격" entities={categoryEntity} />
-
-          <Kernel.Product.FilterGroup name="혜택" entities={categoryEntity} />
-
-          <Kernel.Product.FilterGroup name="유형" entities={categoryEntity} />
+          {filters &&
+            filters.data.data.map(filter => (
+              <Kernel.Product.FilterGroup key={filter.key} name={filter.name} entities={filter.values} expand />
+            ))}
         </Kernel.Product.FilterContainer>
 
         {/* 정렬탭, 아이템(상품) 리스트, 페이지네이션 */}
