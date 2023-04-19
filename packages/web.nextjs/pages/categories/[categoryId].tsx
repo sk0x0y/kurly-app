9dae9f7da12b801eb34646f4de58e4731aa8e29a
sk0x0y <seokyeong.yang@kakao.com>
2023-04-20 03:47:53 +0900

    feat(feature/wwka/category-product): apply category product api

--- packages/web.nextjs/pages/categories/[categoryId].tsx
+++ packages/web.nextjs/pages/categories/[categoryId].tsx
@@ -1,24 +1,22 @@
 import { GetServerSideProps } from 'next';
 import Link from 'next/link';
 import { css } from '@emotion/react';
-import { useSelector } from 'react-redux';
 import Content from '../../components/designsystem/content';
 import Typography from '../../components/designsystem/typography';
 import Product from '../../components/designsystem/product';
 import Kernel from '../../components/designsystem/kernel';
 import Pagination from '../../components/designsystem/pagination/Pagination';
 import ItemSortTab from '../../components/designsystem/tab/ItemSortTab';
-import { RootState } from '../../infrastructure/redux';
-import { useCategory, useCategoryProduct } from '../../hooks';
+import { useCategory, useCategoryProduct, useCategoryProductFilter } from '../../hooks';
 
 interface IProps {
   categoryId: string;
 }
 function CategoryId(props: IProps) {
   const { categoryId } = props;
-  const categoryEntity = useSelector((state: RootState) => state.categoryAdaptor.entity);
 
   const { data: categories } = useCategory(categoryId);
+  const { data: filters } = useCategoryProductFilter(categoryId);
   const { data: products } = useCategoryProduct(categoryId);
 
   // TODO: 추후 react-query 로 API Fetch 해서 Entity 에 Dispatch 해 줄 예정
@@ -108,15 +106,10 @@ function CategoryId(props: IProps) {
         {/* 필터 및 초기화 */}
         <Kernel.Product.FilterContainer>
           {/* 필터 아이템(항목) */}
-          <Kernel.Product.FilterGroup name="카테고리" entities={categoryEntity} expand />
-
-          <Kernel.Product.FilterGroup name="브랜드" entities={categoryEntity} />
-
-          <Kernel.Product.FilterGroup name="가격" entities={categoryEntity} />
-
-          <Kernel.Product.FilterGroup name="혜택" entities={categoryEntity} />
-
-          <Kernel.Product.FilterGroup name="유형" entities={categoryEntity} />
+          {filters &&
+            filters.data.data.map(filter => (
+              <Kernel.Product.FilterGroup key={filter.key} name={filter.name} entities={filter.values} expand />
+            ))}
         </Kernel.Product.FilterContainer>
 
         {/* 정렬탭, 아이템(상품) 리스트, 페이지네이션 */}
@@ -133,7 +126,6 @@ function CategoryId(props: IProps) {
               gap: 31px 18px;
             `}
           >
-            {/* TODO: 새로운 코드에 맞는 리팩터링 필요 */}
             {products &&
               products.data.data.map(product => (
                 <Product.Collection
@@ -149,6 +141,7 @@ function CategoryId(props: IProps) {
     </Content.Section>
   );
 }
+
 export const getServerSideProps: GetServerSideProps = async context => {
   const { query } = context;
   const { categoryId } = query;
