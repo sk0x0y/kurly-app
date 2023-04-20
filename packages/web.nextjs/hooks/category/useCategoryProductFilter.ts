4ef16a7ef4017e4743c304cd624b60fd7bf994a1
sk0x0y <seokyeong.yang@kakao.com>
2023-04-21 06:02:17 +0900

    chore(feature/wwka/category-product): support new parameter subCategoryId

--- packages/web.nextjs/hooks/category/useCategoryProductFilter.ts
+++ packages/web.nextjs/hooks/category/useCategoryProductFilter.ts
@@ -5,19 +5,23 @@ import * as api from '../../api';
 import { RootState } from '../../infrastructure/redux';
 import { IFilter } from '../../infrastructure/interface/filter.interface';
 
-export function useCategoryProductFilter(categoryId: string) {
+export function useCategoryProductFilter(categoryId: string, subCategoryId?: string) {
   const token = useSelector((state: RootState) => state.tokenAdaptor.token);
 
   const result = useQuery<
     AxiosResponse<{
       data: IFilter[];
     }>
-  >(['filter/category-product'], () => api.getCategoryProductFilter(token, categoryId), {
-    onSuccess: res => {
-      console.log('filter/category-product', res.data);
-    },
-    enabled: !!token,
-  });
+  >(
+    ['filter/category-product', categoryId, subCategoryId],
+    () => api.getCategoryProductFilter(token, categoryId, subCategoryId),
+    {
+      onSuccess: res => {
+        console.log('filter/category-product', res.data);
+      },
+      enabled: !!token,
+    }
+  );
 
   return { ...result };
 }
