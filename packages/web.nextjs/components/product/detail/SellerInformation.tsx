629f129a400397bd5c9d282b4605f821e54eac6e
sk0x0y <seokyeong.yang@kakao.com>
2023-04-28 17:49:12 +0900

    feat(develop): add SellerInformation Component for pages/product/[productId].tsx

---
+++ packages/web.nextjs/components/product/detail/SellerInformation.tsx
@@ -0,0 +1,73 @@
+import { css } from '@emotion/react';
+import { IProductDetail } from '../../../infrastructure/interface/product-detail.interface';
+
+interface IProps {
+  product: IProductDetail;
+}
+function SellerInformation(props: IProps) {
+  const { product } = props;
+
+  return (
+    <div
+      css={css`
+        padding: 72px 0;
+        border-top: 1px solid #eee;
+      `}
+    >
+      <h3
+        css={css`
+          font-weight: 500;
+          font-size: 28px;
+          line-height: 41px;
+          text-align: center;
+          letter-spacing: -0.5px;
+          color: #666;
+        `}
+      >
+        판매자 정보
+      </h3>
+
+      <ul
+        css={css`
+          display: flex;
+          flex-wrap: wrap;
+          width: 100%;
+          padding-top: 36px;
+          font-size: 13px;
+          color: #333;
+          line-height: 18px;
+          letter-spacing: -0.5px;
+
+          > li {
+            padding-top: 18px;
+          }
+        `}
+      >
+        <li
+          css={css`
+            width: 180px;
+            padding: 0 18px 18px;
+            background-color: #f7f7f7;
+            word-break: break-all;
+            white-space: pre-line;
+          `}
+        >
+          판매자
+        </li>
+        <li
+          css={css`
+            display: flex;
+            width: 325px;
+            padding: 0 18px 18px;
+            color: #666;
+            white-space: pre-line;
+          `}
+        >
+          {product.sellerName}
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default SellerInformation;
