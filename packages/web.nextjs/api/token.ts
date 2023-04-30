109fff6f8cd5ded2071a03f90efa2668c4401a26
sk0x0y <seokyeong.yang@kakao.com>
2023-05-01 03:05:00 +0900

    fix(develop): fix host when production environment

--- packages/web.nextjs/api/token.ts
+++ packages/web.nextjs/api/token.ts
@@ -2,9 +2,5 @@ import axios from 'axios';
 
 export function getToken() {
   // return axios.get(process.env.NODE_ENV === 'production' ? 'https://www.kurly.com/nx/api/session' : '/nx/api/session');
-  return axios.get(
-    process.env.NODE_ENV === 'production'
-      ? 'https://www.kurly.com/nx/api/session'
-      : 'http://localhost:3000/nx/api/session'
-  );
+  return axios.get(process.env.NODE_ENV === 'production' ? '/nx/api/session' : 'http://localhost:3000/nx/api/session');
 }
