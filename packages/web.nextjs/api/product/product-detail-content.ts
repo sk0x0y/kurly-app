965b015d5f944aa106ed26b50f2385b6264b8e1e
sk0x0y <seokyeong.yang@kakao.com>
2023-05-01 02:58:30 +0900

    fix(develop): User Agent 적용하여 PC버전 응답이 오게끔 수정

--- packages/web.nextjs/api/product/product-detail-content.ts
+++ packages/web.nextjs/api/product/product-detail-content.ts
@@ -1,9 +1,18 @@
 import axios from 'axios';
 
-export function getProductDetailContent(productId: string) {
+export function getProductDetailContent(token: string, productId: string) {
   return axios.get(
     process.env.NODE_ENV === 'production'
       ? `https://www.kurly.com/_next/data/U-R-P-EFAMUfVrHOfIQZ4/goods/${productId}.json`
-      : `http://localhost:3000/ssr/_next/data/U-R-P-EFAMUfVrHOfIQZ4/goods/${productId}.json`
+      : `http://localhost:3000/ssr/_next/data/U-R-P-EFAMUfVrHOfIQZ4/goods/${productId}.json`,
+    {
+      headers: {
+        accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-language': 'ko-KR,ko;q=0.9',
+        'user-agent':
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+      },
+    }
   );
 }
