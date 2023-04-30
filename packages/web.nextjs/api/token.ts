d6ebc89a7ec05c656ce2524b7a0e608cb2a3733e
sk0x0y <seokyeong.yang@kakao.com>
2023-05-01 02:57:23 +0900

    chore(develop): apply conditional host

--- packages/web.nextjs/api/token.ts
+++ packages/web.nextjs/api/token.ts
@@ -2,5 +2,9 @@ import axios from 'axios';
 
 export function getToken() {
   // return axios.get(process.env.NODE_ENV === 'production' ? 'https://www.kurly.com/nx/api/session' : '/nx/api/session');
-  return axios.get('/nx/api/session');
+  return axios.get(
+    process.env.NODE_ENV === 'production'
+      ? 'https://www.kurly.com/nx/api/session'
+      : 'http://localhost:3000/nx/api/session'
+  );
 }
