0ce4ca8634a2b10bb3c29fbe0c11e321b1f8fa85
sk0x0y <seokyeong.yang@kakao.com>
2023-05-08 05:33:55 +0900

    chore(feature/wwka/cart): add cart detail interface

--- packages/web.nextjs/infrastructure/interface/cart.interface.ts
+++ packages/web.nextjs/infrastructure/interface/cart.interface.ts
@@ -6,3 +6,72 @@ export interface ICart {
   salePrice: number;
   deliverFee: number;
 }
+export interface ICartDetail {
+  success: boolean;
+  message: string | null;
+  data: {
+    meta: {
+      legacyCouponMessage: string;
+      isEarlyBird: boolean;
+      activatedContinuity: boolean;
+    };
+    totalCount: number;
+    dealProducts: {
+      masterProductName: string;
+      masterProductCode: string;
+      masterProductNo: number;
+      dealProductName: string;
+      dealProductCode: string;
+      dealProductNo: number;
+      contentsProductName: string;
+      contentsProductCode: string;
+      contentsProductNo: number;
+      imageUrl: string;
+      quantity: number;
+      productPrice: number;
+      discountPrice: number;
+      retailPrice: number;
+      isSoldOut: boolean;
+      isSale: boolean;
+      minQuantity: number;
+      maxQuantity: number | null;
+      minQuantityInContents: number;
+      maxQuantityInContents: number | null;
+      salesUnit: number;
+      storageType: 'COLD';
+      tagNames: null;
+      soldOutText: string;
+      soldOutType: 'COMING_SOON';
+      pointPolicy: 'MEMBER';
+      isNotEnoughStock: boolean;
+      supportDelivery: ('KURLY_PARCEL' | 'KURLY_DAWN')[];
+      deliveryTypeInfos: {
+        deliveryType: 'DAWN' | 'DAY_PARCEL';
+        isDeliveryProduct: boolean;
+      }[];
+      exceptionLabel: null;
+      normalOrderTypePolicyInContents: 'DEFAULT';
+      giftOrderTypePolicyInContents: 'SINGLE_DIRECT_ORDER';
+      deliveryPriceType: 'PAY';
+      createdAt: string;
+      isSingleProduct: boolean;
+      isShow: boolean;
+      couponEndDateText: string;
+      legacyPromotion: null;
+      legacyCoupon: null;
+      isRetired: boolean;
+      isExtraStockSoldOut: boolean;
+      useExtraStockWhenOrder: boolean;
+      activatedExtraStock: boolean;
+      order: {
+        status: 'AVAILABLE';
+        unavailableTypes: [];
+      };
+    }[];
+    displayMessage: {
+      deliveryNotice: {
+        text: '';
+      };
+    };
+  };
+}
