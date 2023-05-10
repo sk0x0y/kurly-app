a8c7c0b9970cd29a354a9169cdfab97181b2dcf2
sk0x0y <seokyeong.yang@kakao.com>
2023-05-10 12:22:36 +0900

    feat(feature/wwka/cart): add new prop, rebuild data structure via Array.reduce(), add conditional rendering with SelectedProductItem Component

--- packages/web.nextjs/components/cart/SelectedProduct.tsx
+++ packages/web.nextjs/components/cart/SelectedProduct.tsx
@@ -1,16 +1,60 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { AxiosResponse } from 'axios';
 import { css } from '@emotion/react';
-import { ICart } from '../../infrastructure/interface/cart.interface';
+import { useCallback } from 'react';
+import { ICart, ICartDetail, ICartProductDetailData } from '../../infrastructure/interface/cart.interface';
 import Button from '../designsystem/button';
 import SelectedProductStoreType from './SelectedProductStoreType';
 import CheckedIcon from '../designsystem/icon/CheckedIcon';
+import { StoreType } from '../../infrastructure/interface/product-detail.interface';
+import SelectedProductItem from './SelectedProductItem';
 
 interface IProps {
   cartEntity: ICart;
+  cartDetail?: AxiosResponse<ICartDetail>;
 }
 function SelectedProduct(props: IProps) {
-  const { cartEntity } = props;
+  const { cartEntity, cartDetail } = props;
+
+  const reducedCartDetail = useCallback(
+    (type?: typeof StoreType[keyof typeof StoreType]) =>
+      cartDetail?.data.data.dealProducts.reduce(
+        (
+          acc: {
+            [StoreType.AMBIENT_TEMPERATURE]: ICartProductDetailData[];
+            [StoreType.COLD]: ICartProductDetailData[];
+            [StoreType.FROZEN]: ICartProductDetailData[];
+          },
+          cur
+        ) => {
+          // 상온 보관
+          if (cur.storageType === StoreType.AMBIENT_TEMPERATURE) {
+            acc = { ...acc, [StoreType.AMBIENT_TEMPERATURE]: [...acc.AMBIENT_TEMPERATURE, cur] };
+          }
+
+          // 냉장 보관
+          if (cur.storageType === StoreType.COLD) {
+            acc = { ...acc, [StoreType.COLD]: [...acc.COLD, cur] };
+          }
+
+          // 냉동 보관
+          if (cur.storageType === StoreType.FROZEN) {
+            acc = { ...acc, [StoreType.FROZEN]: [...acc.FROZEN, cur] };
+          }
+
+          return acc;
+        },
+        {
+          [StoreType.AMBIENT_TEMPERATURE]: [],
+          [StoreType.COLD]: [],
+          [StoreType.FROZEN]: [],
+        }
+      ),
+    []
+  );
+
+  console.log(reducedCartDetail);
 
   return (
     <div
@@ -20,209 +64,41 @@ function SelectedProduct(props: IProps) {
         }
       `}
     >
-      <SelectedProductStoreType type={0} />
+      {/* 상온 상품 */}
+      {!!reducedCartDetail()?.AMBIENT_TEMPERATURE.length && (
+        <>
+          <SelectedProductStoreType type={StoreType.AMBIENT_TEMPERATURE} />
+          <ul>
+            {reducedCartDetail()?.AMBIENT_TEMPERATURE.map(data => (
+              <SelectedProductItem data={data} />
+            ))}
+          </ul>
+        </>
+      )}
 
-      <ul>
-        {cartEntity.selectedProduct.map(entity => (
-          <li
-            key={entity.no}
-            css={css`
-              display: flex;
-              align-items: center;
-              padding: 20px 0;
-            `}
-          >
-            <label
-              css={css`
-                margin-right: 12px;
-              `}
-            >
-              <input
-                css={css`
-                  display: none;
-                `}
-                type="checkbox"
-              />
+      {/* 냉장 상품 */}
+      {!!reducedCartDetail()?.COLD.length && (
+        <>
+          <SelectedProductStoreType type={StoreType.COLD} />
+          <ul>
+            {reducedCartDetail()?.COLD.map(data => (
+              <SelectedProductItem data={data} />
+            ))}
+          </ul>
+        </>
+      )}
 
-              <div>
-                <CheckedIcon width={24} height={24} fill="#5f0080" />
-              </div>
-
-              <span />
-            </label>
-
-            <Link href="/" passHref>
-              <a
-                css={css`
-                  display: inline-block;
-                  width: 60px;
-                  height: 78px;
-                  margin-right: 20px;
-                  background: url(https://res.kurly.com/_next/static/images/noimg-150x195-2c819ff85dca3193dfce31add0852dbb.svg)
-                    center center no-repeat rgb(245, 245, 245);
-                `}
-              >
-                <Image src={entity.mainImageUrl} width={60} height={78} />
-              </a>
-            </Link>
-
-            <div
-              css={css`
-                display: flex;
-                flex-direction: column;
-                width: 345px;
-                margin-right: 20px;
-                overflow: hidden;
-                white-space: nowrap;
-                word-break: break-all;
-              `}
-            >
-              <Link href="/" passHref>
-                <a
-                  css={css`
-                    color: inherit;
-                    text-decoration: inherit;
-                  `}
-                >
-                  <p
-                    css={css`
-                      max-height: 54px;
-                      padding-top: 8px;
-                      font-weight: 500;
-                      font-size: 16px;
-                      line-height: 22px;
-                      display: -webkit-box;
-                      overflow: hidden;
-                      word-break: break-all;
-                      white-space: normal;
-                    `}
-                  >
-                    {entity.name}
-                  </p>
-
-                  <p
-                    css={css`
-                      max-height: 38px;
-                      margin-top: 8px;
-                      margin-bottom: 2px;
-                      font-size: 14px;
-                      line-height: 19px;
-                      color: rgb(153, 153, 153);
-                      display: -webkit-box;
-                      overflow: hidden;
-                      word-break: break-all;
-                      white-space: normal;
-                    `}
-                  >
-                    {entity.masterName}
-                  </p>
-                </a>
-              </Link>
-              <div />
-            </div>
-
-            <div
-              css={css`
-                display: inline-flex;
-                justify-content: space-between;
-              `}
-            >
-              <div
-                css={css`
-                  display: inline-flex;
-                  width: 88px;
-                  border: 1px solid rgb(221, 223, 225);
-                  border-radius: 3px;
-                `}
-              >
-                {/* 선택한 상품 감소 */}
-                <Button.Base
-                  onClick={() => {}}
-                  styles={css`
-                    width: 28px;
-                    height: 28px;
-                    background-size: cover;
-                    background-color: transparent;
-                    background-image: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzAiIGhlaWdodD0iMzAiIHZpZXdCb3g9IjAgMCAzMCAzMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICAgIDxwYXRoIGQ9Ik0yMCAxNHYySDEwdi0yeiIgZmlsbD0iIzMzMyIgZmlsbC1ydWxlPSJub256ZXJvIi8+Cjwvc3ZnPgo=);
-                  `}
-                  nomessage
-                />
-
-                <span
-                  css={css`
-                    font-size: 14px;
-                    text-align: center;
-                    width: 31px;
-                    height: 28px;
-                    line-height: 28px;
-                  `}
-                >
-                  1
-                </span>
-
-                {/* 선택한 상품 증가 */}
-                <Button.Base
-                  onClick={() => {}}
-                  styles={css`
-                    width: 28px;
-                    height: 28px;
-                    background-size: cover;
-                    background-color: transparent;
-                    background-image: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzAiIGhlaWdodD0iMzAiIHZpZXdCb3g9IjAgMCAzMCAzMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICAgIDxwYXRoIGQ9Ik0xNiAxMHY0aDR2MmgtNHY0aC0ydi00aC00di0yaDR2LTRoMnoiIGZpbGw9IiMzMzMiIGZpbGwtcnVsZT0ibm9uemVybyIvPgo8L3N2Zz4K);
-                  `}
-                  nomessage
-                />
-              </div>
-            </div>
-
-            <div
-              css={css`
-                display: flex;
-                flex-direction: column;
-                width: 127px;
-                text-align: right;
-                word-break: break-all;
-              `}
-            >
-              <span
-                css={css`
-                  margin-top: 3px;
-                  font-weight: 700;
-                  font-size: 16px;
-                  color: rgb(51, 51, 51);
-                  line-height: 24px;
-                `}
-                aria-label="판매 가격"
-                data-testid="product-price"
-              >
-                {entity.discountedPrice ? entity.discountedPrice : entity.basePrice}원
-              </span>
-            </div>
-
-            <Button.Base
-              onClick={() => {
-                window.alert('삭제하지마');
-              }}
-              styles={css`
-                width: 30px;
-                height: 30px;
-                margin-left: 9px;
-              `}
-            >
-              <i
-                css={css`
-                  width: 30px;
-                  height: 30px;
-                  display: inline-block;
-                  background-image: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzAiIGhlaWdodD0iMzAiIHZpZXdCb3g9IjAgMCAzMCAzMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICAgIDxwYXRoIGQ9Ik0yMSA5Ljc2MiAyMC4yMzggOSAxNSAxNC4yMzggOS43NjIgOSA5IDkuNzYyIDE0LjIzOCAxNSA5IDIwLjIzOGwuNzYyLjc2MkwxNSAxNS43NjIgMjAuMjM4IDIxbC43NjItLjc2MkwxNS43NjIgMTV6IiBmaWxsPSIjQ0NDIiBmaWxsLXJ1bGU9ImV2ZW5vZGQiLz4KPC9zdmc+Cg==);
-                  background-size: cover;
-                  background-position: center center;
-                `}
-              />
-            </Button.Base>
-          </li>
-        ))}
-      </ul>
+      {/* 냉동 상품 */}
+      {!!reducedCartDetail()?.FROZEN.length && (
+        <>
+          <SelectedProductStoreType type={StoreType.FROZEN} />
+          <ul>
+            {reducedCartDetail()?.FROZEN.map(data => (
+              <SelectedProductItem data={data} />
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 }
