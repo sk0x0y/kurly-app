263c80d6c823356650660ddd78826e4c99bdfd01
sk0x0y <seokyeong.yang@kakao.com>
2023-05-15 21:02:33 +0900

    refactor(feature/wwka/cart): Cart 엔티티에 useCartDetail 값을 담을 수 있는 새로운 필드 생성

--- packages/web.nextjs/application/entities/cart.entity.ts
+++ packages/web.nextjs/application/entities/cart.entity.ts
@@ -1,7 +1,8 @@
-import { ICart } from '../../infrastructure/interface/cart.interface';
+import { ICart, ICartDetail } from '../../infrastructure/interface/cart.interface';
 
 export class CartEntity {
   entity: ICart;
+  detail: ICartDetail;
 
   constructor() {
     this.entity = {
@@ -9,6 +10,24 @@ export class CartEntity {
       productPrice: 0,
       salePrice: 0,
       deliverFee: 0,
+    };
+    this.detail = {
+      data: {
+        dealProducts: [],
+        displayMessage: {
+          deliveryNotice: {
+            text: '',
+          },
+        },
+        meta: {
+          activatedContinuity: false,
+          isEarlyBird: false,
+          legacyCouponMessage: '',
+        },
+        totalCount: 0,
+      },
+      message: null,
+      success: false,
     };
   }
 }
