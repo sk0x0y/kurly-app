b5ace3fad9b14ece80b150c0b840978cf062ca24
sk0x0y <seokyeong.yang@kakao.com>
2023-05-15 20:57:28 +0900

    refactor(feature/wwka/cart): 일부 로직, UI 등을 새로운 SelectedProductList 컴포넌트로 분리

--- packages/web.nextjs/components/cart/SelectedProduct.tsx
+++ packages/web.nextjs/components/cart/SelectedProduct.tsx
@@ -1,60 +1,16 @@
-import Link from 'next/link';
-import Image from 'next/image';
 import { AxiosResponse } from 'axios';
 import { css } from '@emotion/react';
-import { useCallback } from 'react';
-import { ICart, ICartDetail, ICartProductDetailData } from '../../infrastructure/interface/cart.interface';
-import Button from '../designsystem/button';
-import SelectedProductStoreType from './SelectedProductStoreType';
-import CheckedIcon from '../designsystem/icon/CheckedIcon';
+import { ICartDetail } from '../../infrastructure/interface/cart.interface';
 import { StoreType } from '../../infrastructure/interface/product-detail.interface';
-import SelectedProductItem from './SelectedProductItem';
+
+import SelectedProductList from './SelectedProductList';
 
 interface IProps {
-  cartEntity: ICart;
+  cartEntity: ICartDetail;
   cartDetail?: AxiosResponse<ICartDetail>;
 }
 function SelectedProduct(props: IProps) {
   const { cartEntity, cartDetail } = props;
-
-  const reducedCartDetail = useCallback(
-    (type?: typeof StoreType[keyof typeof StoreType]) =>
-      cartDetail?.data.data.dealProducts.reduce(
-        (
-          acc: {
-            [StoreType.AMBIENT_TEMPERATURE]: ICartProductDetailData[];
-            [StoreType.COLD]: ICartProductDetailData[];
-            [StoreType.FROZEN]: ICartProductDetailData[];
-          },
-          cur
-        ) => {
-          // 상온 보관
-          if (cur.storageType === StoreType.AMBIENT_TEMPERATURE) {
-            acc = { ...acc, [StoreType.AMBIENT_TEMPERATURE]: [...acc.AMBIENT_TEMPERATURE, cur] };
-          }
-
-          // 냉장 보관
-          if (cur.storageType === StoreType.COLD) {
-            acc = { ...acc, [StoreType.COLD]: [...acc.COLD, cur] };
-          }
-
-          // 냉동 보관
-          if (cur.storageType === StoreType.FROZEN) {
-            acc = { ...acc, [StoreType.FROZEN]: [...acc.FROZEN, cur] };
-          }
-
-          return acc;
-        },
-        {
-          [StoreType.AMBIENT_TEMPERATURE]: [],
-          [StoreType.COLD]: [],
-          [StoreType.FROZEN]: [],
-        }
-      ),
-    []
-  );
-
-  console.log(reducedCartDetail);
 
   return (
     <div
@@ -64,41 +20,14 @@ function SelectedProduct(props: IProps) {
         }
       `}
     >
-      {/* 상온 상품 */}
-      {!!reducedCartDetail()?.AMBIENT_TEMPERATURE.length && (
-        <>
-          <SelectedProductStoreType type={StoreType.AMBIENT_TEMPERATURE} />
-          <ul>
-            {reducedCartDetail()?.AMBIENT_TEMPERATURE.map(data => (
-              <SelectedProductItem data={data} />
-            ))}
-          </ul>
-        </>
-      )}
-
       {/* 냉장 상품 */}
-      {!!reducedCartDetail()?.COLD.length && (
-        <>
-          <SelectedProductStoreType type={StoreType.COLD} />
-          <ul>
-            {reducedCartDetail()?.COLD.map(data => (
-              <SelectedProductItem data={data} />
-            ))}
-          </ul>
-        </>
-      )}
+      <SelectedProductList cartEntity={cartEntity} type={StoreType.COLD} />
 
       {/* 냉동 상품 */}
-      {!!reducedCartDetail()?.FROZEN.length && (
-        <>
-          <SelectedProductStoreType type={StoreType.FROZEN} />
-          <ul>
-            {reducedCartDetail()?.FROZEN.map(data => (
-              <SelectedProductItem data={data} />
-            ))}
-          </ul>
-        </>
-      )}
+      <SelectedProductList cartEntity={cartEntity} type={StoreType.FROZEN} />
+
+      {/* 상온 상품 */}
+      <SelectedProductList cartEntity={cartEntity} type={StoreType.AMBIENT_TEMPERATURE} />
     </div>
   );
 }
---
+++ packages/web.nextjs/components/cart/SelectedProductList.tsx
@@ -0,0 +1,65 @@
+import { Fragment, useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { StoreType } from '../../infrastructure/interface/product-detail.interface';
+import { ICartDetail, ICartProductDetailData } from '../../infrastructure/interface/cart.interface';
+import { actions } from '../../application/usecases/cart.usecase';
+import SelectedProductStoreType from './SelectedProductStoreType';
+import SelectedProductItem from './SelectedProductItem';
+
+interface IProps {
+  cartEntity: ICartDetail;
+  type: typeof StoreType[keyof typeof StoreType];
+}
+function SelectedProductList(props: IProps) {
+  const { cartEntity, type } = props;
+
+  const dispatch = useDispatch();
+
+  const reducedCartDetail = useCallback(
+    () =>
+      cartEntity?.data.dealProducts.reduce(
+        (
+          acc: {
+            [StoreType.AMBIENT_TEMPERATURE]: ICartProductDetailData[];
+            [StoreType.COLD]: ICartProductDetailData[];
+            [StoreType.FROZEN]: ICartProductDetailData[];
+          },
+          cur
+        ) => {
+          if (cur.storageType === StoreType[type]) {
+            acc = { ...acc, [type]: [...acc[type], cur] };
+          }
+
+          return acc;
+        },
+        {
+          [StoreType.AMBIENT_TEMPERATURE]: [],
+          [StoreType.COLD]: [],
+          [StoreType.FROZEN]: [],
+        }
+      ),
+    [cartEntity?.data.dealProducts, type]
+  );
+
+  if (reducedCartDetail()?.[type].length) {
+    return (
+      <>
+        <SelectedProductStoreType type={StoreType[type]} />
+
+        {reducedCartDetail()?.[type].map(data => (
+          <SelectedProductItem
+            key={data.dealProductNo}
+            handleRemove={() => {
+              dispatch(actions.remove(data.dealProductNo));
+            }}
+            data={data}
+          />
+        ))}
+      </>
+    );
+  }
+
+  return null;
+}
+
+export default SelectedProductList;
